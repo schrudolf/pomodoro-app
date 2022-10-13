67a2c69173d704c6b5aab19b2cec234ebf4892ee
Rudolf <spureszka@gmail.com>
2022-10-13 19:18:26 +0200

    add workSlider Component

--- pomodoro-app/src/App.tsx
+++ pomodoro-app/src/App.tsx
@@ -1,10 +1,24 @@
-import React from 'react'
-import './App.css'
+import React from "react";
+import "./App.css";
+
+import Box from "@mui/material/Box";
+import Container from "@mui/material/Container";
+import InputLabel from "@mui/material/InputLabel";
+import FormControl from "@mui/material/FormControl";
+import NativeSelect from "@mui/material/NativeSelect";
+
+import Button from "@mui/material/Button";
+
+import WorkSlider from "./components/workSlider";
 
 function App() {
   return (
-    <h1>Pomodoro-app</h1>
-  )
+    <Container>
+      <Box sx={{ width: "50%", margin: "auto" }}>
+        asdasd<WorkSlider />
+      </Box>
+    </Container>
+  );
 }
 
-export default App
+export default App;
---
+++ pomodoro-app/src/components/workSlider.tsx
@@ -0,0 +1,94 @@
+import * as React from "react";
+import PropTypes from "prop-types";
+import Slider, { SliderThumb } from "@mui/material/Slider";
+import { styled } from "@mui/material/styles";
+import Typography from "@mui/material/Typography";
+import Tooltip from "@mui/material/Tooltip";
+import Box from "@mui/material/Box";
+
+const marks = (function () {
+  let getWorkTimers = [];
+  for (let i = 1; i <= 24; i++) {
+    if (i % 5 === 0 || i === 1 || i === 24) {
+      getWorkTimers.push({
+        value: i * 5,
+        label: `${0 + i * 5}:00`,
+      });
+    }
+  }
+  return getWorkTimers;
+})();
+
+const iOSBoxShadow =
+  "0 3px 1px rgba(0,0,0,0.1),0 4px 8px rgba(0,0,0,0.13),0 0 0 1px rgba(0,0,0,0.02)";
+
+const IOSSlider = styled(Slider)(({ theme }) => ({
+  color: theme.palette.mode === "dark" ? "#3880ff" : "#3880ff",
+  height: 2,
+  padding: "15px 0",
+  "& .MuiSlider-thumb": {
+    height: 28,
+    width: 28,
+    backgroundColor: "#fff",
+    boxShadow: iOSBoxShadow,
+    "&:focus, &:hover, &.Mui-active": {
+      boxShadow:
+        "0 3px 1px rgba(0,0,0,0.1),0 4px 8px rgba(0,0,0,0.3),0 0 0 1px rgba(0,0,0,0.02)",
+      // Reset on touch devices, it doesn't add specificity
+      "@media (hover: none)": {
+        boxShadow: iOSBoxShadow,
+      },
+    },
+  },
+  "& .MuiSlider-valueLabel": {
+    fontSize: 12,
+    fontWeight: "normal",
+    top: -6,
+    backgroundColor: "unset",
+    color: theme.palette.text.primary,
+    "&:before": {
+      display: "none",
+    },
+    "& *": {
+      background: "transparent",
+      color: theme.palette.mode === "dark" ? "#fff" : "#000",
+    },
+  },
+  "& .MuiSlider-track": {
+    border: "none",
+  },
+  "& .MuiSlider-rail": {
+    opacity: 0.5,
+    backgroundColor: "#bfbfbf",
+  },
+  "& .MuiSlider-mark": {
+    backgroundColor: "#bfbfbf",
+    height: 8,
+    width: 1,
+    "&.MuiSlider-markActive": {
+      opacity: 1,
+      backgroundColor: "currentColor",
+    },
+  },
+}));
+
+const WorkSlider = () => {
+  return (
+    <div>
+      <Typography  sx={{margin: "1rem 50% 1.5rem 50%"}} gutterBottom>Work</Typography>
+      <IOSSlider
+        aria-label="ios slider"
+        defaultValue={10}
+        step={5}
+        min={5}
+        max={120}
+        marks={marks}
+        valueLabelDisplay="on"
+        valueLabelFormat={(value) => `${value}:00 minutes`}
+      />
+      <Box sx={{ m: 3 }} />
+    </div>
+  );
+};
+
+export default WorkSlider;
