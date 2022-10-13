7fc6be0661a6b68ae99e10db83d6acd3c90d7155
Rudolf <spureszka@gmail.com>
2022-10-13 20:03:02 +0200

    style in external component

---
+++ pomodoro-app/src/components/slidesStyle.tsx
@@ -0,0 +1,60 @@
+import { styled } from "@mui/material/styles";
+import Slider from "@mui/material/Slider";
+
+const slideBoxShadow =
+  "0 3px 1px rgba(0,0,0,0.1),0 4px 8px rgba(0,0,0,0.13),0 0 0 1px rgba(0,0,0,0.02)";
+
+
+const SlideStyle = styled(Slider)(({ theme }) => ({
+  color: "lightgreen",
+  height: 2,
+  padding: "15px 0",
+  "& .MuiSlider-thumb": {
+    height: 28,
+    width: 28,
+    backgroundColor: "white",
+    boxShadow: slideBoxShadow,
+    "&:focus, &:hover, &.Mui-active": {
+      boxShadow:
+        "0 3px 1px rgba(0,0,0,0.1),0 4px 8px rgba(0,0,0,0.3),0 0 0 1px rgba(0,0,0,0.02)",
+      // Reset on touch devices, it doesn't add specificity
+      "@media (hover: none)": {
+        boxShadow: slideBoxShadow,
+      },
+    },
+  },
+  "& .MuiSlider-valueLabel": {
+    fontSize: 12,
+    fontWeight: "normal",
+    top: -6,
+    backgroundColor: "unset",
+    color: theme.palette.text.primary,
+    "&:before": {
+      display: "none",
+    },
+    "& *": {
+      background: "transparent",
+      color: "white",
+      fontSize: "1rem",
+    },
+  },
+  "& .MuiSlider-track": {
+    border: "none",
+  },
+  "& .MuiSlider-rail": {
+    opacity: 0.5,
+    backgroundColor: "white",
+  },
+  "& .MuiSlider-mark": {
+    backgroundColor: "black",
+    height: 8,
+    width: 1,
+    "&.MuiSlider-markActive": {
+      opacity: 1,
+      backgroundColor: "currentColor",
+    },
+  },
+}));
+
+
+export default SlideStyle;
--- pomodoro-app/src/components/workSlider.tsx
+++ pomodoro-app/src/components/workSlider.tsx
@@ -1,10 +1,8 @@
 import * as React from "react";
-import PropTypes from "prop-types";
-import Slider, { SliderThumb } from "@mui/material/Slider";
-import { styled } from "@mui/material/styles";
 import Typography from "@mui/material/Typography";
-import Tooltip from "@mui/material/Tooltip";
 import Box from "@mui/material/Box";
+
+import SlideStyle from "./slidesStyle";
 
 const marks = (function () {
   let getWorkTimers = [];
@@ -19,70 +17,16 @@ const marks = (function () {
   return getWorkTimers;
 })();
 
-const iOSBoxShadow =
-  "0 3px 1px rgba(0,0,0,0.1),0 4px 8px rgba(0,0,0,0.13),0 0 0 1px rgba(0,0,0,0.02)";
-
-const IOSSlider = styled(Slider)(({ theme }) => ({
-  color: "lightgreen",
-  height: 2,
-  padding: "15px 0",
-  "& .MuiSlider-thumb": {
-    height: 28,
-    width: 28,
-    backgroundColor: "white",
-    boxShadow: iOSBoxShadow,
-    "&:focus, &:hover, &.Mui-active": {
-      boxShadow:
-        "0 3px 1px rgba(0,0,0,0.1),0 4px 8px rgba(0,0,0,0.3),0 0 0 1px rgba(0,0,0,0.02)",
-      // Reset on touch devices, it doesn't add specificity
-      "@media (hover: none)": {
-        boxShadow: iOSBoxShadow,
-      },
-    },
-  },
-  "& .MuiSlider-valueLabel": {
-    fontSize: 12,
-    fontWeight: "normal",
-    top: -6,
-    backgroundColor: "unset",
-    color: theme.palette.text.primary,
-    "&:before": {
-      display: "none",
-    },
-    "& *": {
-      background: "transparent",
-      color: "white",
-      fontSize: "1rem",
-    },
-  },
-  "& .MuiSlider-track": {
-    border: "none",
-  },
-  "& .MuiSlider-rail": {
-    opacity: 0.5,
-    backgroundColor: "white",
-  },
-  "& .MuiSlider-mark": {
-    backgroundColor: "black",
-    height: 8,
-    width: 1,
-    "&.MuiSlider-markActive": {
-      opacity: 1,
-      backgroundColor: "currentColor",
-    },
-  },
-}));
-
 const WorkSlider = () => {
   return (
     <div>
       <Typography
-        sx={{ margin: "1rem 0 1.5rem -2.5rem", fontWeight: 600, width: "100%" }}
+        sx={{ margin: "1rem 0 2rem -2.5rem", fontWeight: 600, width: "100%" }}
         gutterBottom
       >
         Work time:
       </Typography>
-      <IOSSlider
+      <SlideStyle
         aria-label="ios slider"
         defaultValue={10}
         step={5}
