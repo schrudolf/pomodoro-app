47a0d304c69dca0803a4bb7679beb88fad0d115f
Rudolf <spureszka@gmail.com>
2022-10-20 15:45:17 +0200

    start/pause/continue/reset status handlers

--- pomodoro-app/src/components/pomodoro.tsx
+++ pomodoro-app/src/components/pomodoro.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 
 import Box from "@mui/material/Box";
 import Container from "@mui/material/Container";
@@ -14,6 +14,57 @@ const Pomodoro = (props: {
   setSettingStatus: (value: Boolean) => void;
   settings: AppSettings;
 }) => {
+  const [pomodoroStatus, setPomodoroStatus] = useState(0);
+  const getHandlerButton = (status: number) => {
+    if (status === 0) {
+      return (
+        <Button
+          onClick={startPomodoroApp}
+          variant="contained"
+          sx={{ width: "30%", margin: "0.3rem", fontWeight: 600 }}
+        >
+          Start
+        </Button>
+      );
+    } else if (status === 1) {
+      return (
+        <Button
+          onClick={pausePomodoroApp}
+          variant="contained"
+          color="error"
+          sx={{ width: "30%", margin: "0.3rem", fontWeight: 600 }}
+        >
+          Pause
+        </Button>
+      );
+    } else {
+      return (
+        <Button
+          onClick={continuePomodoroApp}
+          variant="contained"
+          color="success"
+          sx={{ width: "30%", margin: "0.3rem", fontWeight: 600 }}
+        >
+          Continue
+        </Button>
+      );
+    }
+  };
+  const startPomodoroApp = () => {
+    setPomodoroStatus(1);
+  };
+  const pausePomodoroApp = () => {
+    setPomodoroStatus(2);
+  };
+  const continuePomodoroApp = () => {
+    setPomodoroStatus(1);
+  };
+  const resetPomodoroApp = () => {
+    setPomodoroStatus(0);
+  };
+  // const pomodoroStatusHandler = () => {
+  //   setPomodoroStatus(!pomodoroStatus);
+  // };
   const openSettings = () => {
     props.setSettingStatus(true);
   };
@@ -36,10 +87,13 @@ const Pomodoro = (props: {
         </Typography>
       </Box>
       <Box textAlign={"center"}>
-        <Button variant="contained" sx={{ width: "30%", margin: "0.3rem" }}>
-          Start
-        </Button>
-        <Button variant="contained" sx={{ width: "30%", margin: "0.3rem" }}>
+        {getHandlerButton(pomodoroStatus)}
+        <Button
+          variant="contained"
+          color="warning"
+          onClick={resetPomodoroApp}
+          sx={{ width: "30%", margin: "0.3rem", fontWeight: 600 }}
+        >
           Reset
         </Button>
       </Box>
@@ -49,10 +103,13 @@ const Pomodoro = (props: {
           variant="body2"
           onClick={openSettings}
           title={"Settings"}
-          sx={{ fontSize: "3rem", color: "white", textDecoration: "none", }}
+          sx={{ fontSize: "3rem", color: "white", textDecoration: "none" }}
         >
-          <SettingsIcon sx={{ fontSize: "1rem", color: "white", marginRight: "4px" }} />
-          <Typography fontSize={20} display={"inline"} alignContent={"center"} >Settings
+          <SettingsIcon
+            sx={{ fontSize: "1rem", color: "white", marginRight: "4px" }}
+          />
+          <Typography fontSize={20} display={"inline"} alignContent={"center"}>
+            Settings
           </Typography>
         </Link>
       </Box>
